94a6112ad338bd5987a8db79879f7b170fef2a24
emre gemici <gmcc.emree@gmail.com>
2021-12-08 20:52:25 +0300

    Add project files.

---
+++ MCD_ModelModelLess/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace MCD_ModelModelLess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.btnGosterPopup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGoster.Location = new System.Drawing.Point(317, 117);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(216, 33);
+            this.btnGoster.TabIndex = 0;
+            this.btnGoster.Text = "Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // btnGosterPopup
+            // 
+            this.btnGosterPopup.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGosterPopup.Location = new System.Drawing.Point(317, 187);
+            this.btnGosterPopup.Name = "btnGosterPopup";
+            this.btnGosterPopup.Size = new System.Drawing.Size(216, 35);
+            this.btnGosterPopup.TabIndex = 1;
+            this.btnGosterPopup.Text = "Göster Popup";
+            this.btnGosterPopup.UseVisualStyleBackColor = true;
+            this.btnGosterPopup.Click += new System.EventHandler(this.btnGosterPopup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 600);
+            this.Controls.Add(this.btnGosterPopup);
+            this.Controls.Add(this.btnGoster);
+            this.Name = "Form1";
+            this.Text = "Ana Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.Button btnGosterPopup;
+    }
+}
+
---
+++ MCD_ModelModelLess/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCD_ModelModelLess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            MusteriKayit musteriKayit = new MusteriKayit();
+            musteriKayit.Show();
+            //Yeni form oluşturmak için kullanılır birden fazla kez tıklanabilir veya kullanıcı form açık iken uygulamanın diğer menülerine erişebilir.
+        }
+
+        private void btnGosterPopup_Click(object sender, EventArgs e)
+        {
+            MusteriKayit musteriKayit = new MusteriKayit();
+            musteriKayit.ShowDialog();
+            //Kullanıcı bu şekilde form açtığı zaman ilgili form kapanmadan herhangi bir işlem yapamaz.
+        }
+    }
+}
